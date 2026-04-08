4292a95946d08b135411b037c99fe7bfb11a42b8
AlteredProject <125555532+AlteredProject@users.noreply.github.com>
2026-04-08 12:26:48 +0200

    Initial site setup

---
+++ src/main/java/app/controllers/MainController.java
@@ -0,0 +1,10 @@
+package app.controllers;
+
+import app.persistence.ConnectionPool;
+import io.javalin.Javalin;
+
+public class MainController {
+    public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
+        app.get("/", ctx -> ctx.render("index.html"));
+    }
+}

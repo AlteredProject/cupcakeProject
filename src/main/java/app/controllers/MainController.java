8756acce7ae02040a0d0ceb6092f099b4781ed64
BertramFM <160260021+BertramFM@users.noreply.github.com>
2026-04-09 15:01:31 +0200

    added homepage() so i can get topping&bottom list in th

--- src/main/java/app/controllers/MainController.java
+++ src/main/java/app/controllers/MainController.java
@@ -1,10 +1,25 @@
 package app.controllers;
 
+import app.entities.Bottoms;
+import app.entities.Toppings;
 import app.persistence.ConnectionPool;
+import app.persistence.CupcakeMapper;
 import io.javalin.Javalin;
+import io.javalin.http.Context;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 public class MainController {
     public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
-        app.get("/", ctx -> ctx.render("index.html"));
+        app.get("/", ctx -> homePage(ctx, connectionPool));
+    }
+
+    private static void homePage(@NotNull Context ctx, ConnectionPool connectionPool){
+        List<Toppings> toppings = CupcakeMapper.getAllToppings(connectionPool);
+        List<Bottoms> bottoms = CupcakeMapper.getAllBottoms(connectionPool);
+        ctx.attribute("Toppings", toppings);
+        ctx.attribute("Bottoms", bottoms);
+        ctx.render("index.html");
     }
 }

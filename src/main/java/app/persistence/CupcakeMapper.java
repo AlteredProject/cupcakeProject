1ab4bc376d1f2303a7a08d960276881a80452d29
BertramFM <160260021+BertramFM@users.noreply.github.com>
2026-04-13 14:19:14 +0200

    added 2 methods to get bottom/topping by id

--- src/main/java/app/persistence/CupcakeMapper.java
+++ src/main/java/app/persistence/CupcakeMapper.java
@@ -54,4 +54,50 @@ public class CupcakeMapper {
             throw new RuntimeException(e);
         }
     }
+
+    public static Toppings getToppingById(int id, ConnectionPool conn){
+        Toppings topping = null;
+        String sql_toppingById = "SELECT * FROM cupcake_toppings\n" +
+                "WHERE topping_id = ?";
+
+        try(
+                Connection connection = conn.getConnection();
+                PreparedStatement ps = connection.prepareStatement(sql_toppingById);
+        ) {
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                int toppingId = rs.getInt("topping_id");
+                String name = rs.getString("name");
+                int price = rs.getInt("price");
+                topping = new Toppings(toppingId, name, price);
+            }
+            return topping;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Bottoms getBottomById(int id, ConnectionPool conn){
+        Bottoms bottom = null;
+        String sql_bottomById = "SELECT * FROM cupcake_bottoms\n" +
+                "WHERE bottom_id = ?";
+
+        try(
+                Connection connection = conn.getConnection();
+                PreparedStatement ps = connection.prepareStatement(sql_bottomById);
+        ) {
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                int bottomId = rs.getInt("topping_id");
+                String name = rs.getString("name");
+                int price = rs.getInt("price");
+                bottom = new Bottoms(bottomId, name, price);
+            }
+            return bottom;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }

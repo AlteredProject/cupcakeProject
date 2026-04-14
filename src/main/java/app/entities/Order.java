bea707cf788f096e3460a2a111f1d1dbfea33082
BertramFM <160260021+BertramFM@users.noreply.github.com>
2026-04-14 14:16:20 +0200

    updated

--- src/main/java/app/entities/Order.java
+++ src/main/java/app/entities/Order.java
@@ -1,4 +1,49 @@
 package app.entities;
 
+import java.sql.Date;
+
 public class Order {
+    private int orderId;
+    private int userId;
+    private Date orderDate;
+    private double totalPrice;
+
+    public Order(int orderId, int userId, Date orderDate, double totalPrice) {
+        this.orderId = orderId;
+        this.userId = userId;
+        this.orderDate = orderDate;
+        this.totalPrice = totalPrice;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
 }

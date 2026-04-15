5e54e37885e2e3162591c29b348d4e3a4de620b5
AlteredProject <125555532+AlteredProject@users.noreply.github.com>
2026-04-15 12:14:47 +0200

    Added order summary page for admin view

--- src/main/java/app/controllers/MainController.java
+++ src/main/java/app/controllers/MainController.java
@@ -1,12 +1,14 @@
 package app.controllers;
 
 import app.dto.CustomerInfoDTO;
+import app.dto.OrderSummaryDTO;
 import app.entities.Bottoms;
 import app.entities.Toppings;
 import app.exceptions.DatabaseException;
 import app.persistence.ConnectionPool;
 import app.persistence.CupcakeMapper;
 import app.persistence.CustomerMapper;
+import app.persistence.OrderMapper;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 import org.jetbrains.annotations.NotNull;
@@ -35,13 +37,20 @@ public class MainController {
 
             ctx.attribute("customers", customers);
             ctx.render("customers-admin.html");
-
         } catch (DatabaseException e) {
             ctx.result("Fejl ved indhentning af kunder");
         }
     }
 
     public static void adminOrdersPage(@NotNull Context ctx, ConnectionPool connectionPool) {
+        try {
+           List<OrderSummaryDTO> orderSummaries = OrderMapper.getAllOrders(connectionPool);
+
+           ctx.attribute("orders", orderSummaries);
+           ctx.render("orders-admin.html");
+        } catch (DatabaseException e) {
+            ctx.result("Fejl ved indhentning af ordrer");
+        }
         ctx.render("orders-admin.html");
     }
 }
